be0fde9bfb26a68fec5ffc1156a00f3c2bc350d6
Mark McGuire <mark.mcguire@libertymutual.com>
2025-07-18 12:50:28 -0400

    find button working

--- public/index.js
+++ public/index.js
@@ -16,3 +16,18 @@ function clearAllFields() {
         document.getElementById("ID").value = '';
         document.getElementById("email").value = '';
     }
+
+document.addEventListener('DOMContentLoaded', function() {
+    var iDinputField = document.getElementById('ID');
+    var eMailInputField = document.getElementById('email');
+    var linkElement = document.getElementById('findLink');
+
+    iDinputField.addEventListener('input', function() {
+        var inputValue = iDinputField.value; 
+        linkElement.href = "/customers/find?id="+inputValue; // Update the href with the input value
+    });
+    eMailInputField.addEventListener('input', function() {
+        var inputValue = eMailInputField.value; 
+        linkElement.href = "/customers/find?email="+inputValue;
+    });    
+});    
